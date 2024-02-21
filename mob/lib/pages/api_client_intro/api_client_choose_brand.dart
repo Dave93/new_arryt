e7eed6aac36ba9808b7a33322c89e98d7a58cfe7
Davr <yul.davron.93@gmail.com>
2024-02-21 20:18:12 +0500

    finished working on all sections

--- mob/lib/pages/api_client_intro/api_client_choose_brand.dart
+++ mob/lib/pages/api_client_intro/api_client_choose_brand.dart
@@ -33,7 +33,7 @@ class _ApiClientChooseBrandState extends State<ApiClientChooseBrand> {
     ApiServer api = new ApiServer();
 
     // get brands from api
-    Response response = await api.get('/api/brands/cached');
+    Response response = await api.get('/api/brands/cached', {});
     print(response.data['data']);
     // var brands = response.data['data']['brands'] as List<dynamic>;
     // create http graphql request to get brands
