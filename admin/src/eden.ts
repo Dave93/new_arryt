32acf251761e84f94a0fa00e1071ea8e9665147f
Davr <yul.davron.93@gmail.com>
2024-06-04 14:40:03 +0500

    445

--- admin/src/eden.ts
+++ admin/src/eden.ts
@@ -1,7 +1,7 @@
 import { edenTreaty, edenFetch } from "@elysiajs/eden";
 import type { BackendApp } from "@api/src/index";
 
-console.log('api url')
+console.log('api url', import.meta.env.VITE_API_URL!)
 
 export const apiClient = edenTreaty<BackendApp>(import.meta.env.VITE_API_URL!);
 export const apiFetch = edenFetch<BackendApp>(import.meta.env.VITE_API_URL!);
