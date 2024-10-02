24b2a1a7b2df787b7d4ff536e303fd9dc160d056
Davr <yul.davron.93@gmail.com>
2024-10-02 18:57:17 +0500

    dd

--- admin/src/eden.ts
+++ admin/src/eden.ts
@@ -3,6 +3,8 @@ import type { BackendApp } from "@api/src/app";
 
 
 export const apiClient = edenTreaty<BackendApp>(import.meta.env.VITE_API_URL!, {
-    credentials: "include",
+    fetch: {
+        credentials: "include",
+    }
 });
 export const apiFetch = edenFetch<BackendApp>(import.meta.env.VITE_API_URL!);
